d11a31028a393e77e05e4c45f76553129f031f5e
Jacob Larsen <jacobniclarsen@gmail.com>
2018-09-07 00:03:40 +0200

    Oppgave 9 ferdig 1.3.1

--- src/hjelpeklasser/Tabell.java
+++ src/hjelpeklasser/Tabell.java
@@ -453,4 +453,43 @@ public class Tabell {
         }
     }
 
+
+    /**
+     * Sorterer tabellen med utvalg
+     * @param a Array
+     */
+    public static void utvalgssortering(int[] a)
+    {
+        for (int i = 0; i < a.length - 1; i++)
+            bytt(a, i, min(a, i, a.length));  // to hjelpemetoder
+    }
+
+    public static void utvalgsortInternett(int arr[])
+    {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void utvalgssortering(int[] a, int fra, int til){
+
+        for(int i = fra; i<til; i++){
+            bytt(a,i,min(a,i,til));
+        }
+    }
+
 }
