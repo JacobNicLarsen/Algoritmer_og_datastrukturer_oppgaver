0e6d8a5b0445cf58786dd1fb5eaac0b8ddafaaf5
Jacob Larsen <jacobniclarsen@gmail.com>
2018-09-17 19:58:48 +0200

    uke 4 oppgave 3

--- src/hjelpeklasser/Main.java
+++ src/hjelpeklasser/Main.java
@@ -9,13 +9,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] a = Tabell.randPerm(20); // en tabell
-        int antall = 10;                                   // antall verdier
+        //System.out.println(a1(0));
+        System.out.println(1234%10);
+        System.out.println(tverrsum(1234));
 
 
-        Tabell.innsettingssortering(a,2,10);
+    }
 
-        System.out.println(Arrays.toString(a));
+    public static int a(int n)           // n må være et ikke-negativt tall
+    {
+        if (n == 0) return 1;              // a0 = 1
+        else if (n == 1) return 2;         // a1 = 2
+        else return 2*a(n-1) + 3*a(n-2);   // to rekursive kall
+    }
 
+    public static int a1(int n){
+        if(n<0) throw new IllegalArgumentException("n må være større enn 0");
+
+        int x = 0, y = 1, z = 1;
+
+        for (int i = 0; i < n; i++) {
+            z = 2*y + 3*x;
+            x = y;
+            y = z;
+        }
+
+        return z;
+    }
+
+    public static int tverrsum(int n){
+
+        int sum = 0;
+
+        while (n > 0){
+            sum += n%10;
+            n /= 10;
+
+        }
+        return sum;
     }
 }
